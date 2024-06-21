f74d4a9d49b94f1512a860a1b817db139fa12e36
Mark Colley <39366189+M-Colley@users.noreply.github.com>
2024-06-21 08:33:13 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -22,8 +22,8 @@ setup(
   long_description=long_description,
   long_description_content_type='text/markdown',
   author = 'Mark Colley',                   # Type in your name
-  author_email = 'todo@yahoo.de',      # Type in your E-Mail
-  url = 'TODO',   # Provide either the link to your github or to your website
+  author_email = 'mark.colley@yahoo.de',      # Type in your E-Mail
+  url = 'm-colley.github.io',   # Provide either the link to your github or to your website
   download_url = '',   # I explain this later on
   keywords = ['simulation', 'pedestrian', 'automated-vehicles', 'ehmi', 'llm'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
@@ -34,18 +34,16 @@ setup(
           'matplotlib',
           'transformers',
           'torch',
-          'torchvision',
-          'torchaudio'
       ],
   include_package_data=True,
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
-    'Intended Audience :: Developers',      # Define that your audience are developers
+    'Intended Audience :: Developers',      # Define that your audience is developers
     'Topic :: Software Development :: Build Tools',
     'License :: OSI Approved :: MIT License',   # Again, pick a license
-    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
-    'Programming Language :: Python :: 3.9',
+    'Programming Language :: Python :: 3',      #Specify which python versions that you want to support
     'Programming Language :: Python :: 3.10',
     'Programming Language :: Python :: 3.11',
+    'Programming Language :: Python :: 3.12',
   ],
 )
